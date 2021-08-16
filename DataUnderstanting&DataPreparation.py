0a3ae5f818153ceb6acf6cfe3c88f1db72463ea8
nacarmonare <42666023+nacarmonare@users.noreply.github.com>
2021-08-15 20:51:31 -0500

    DataSet

--- DataUnderstanting&DataPreparation.py
+++ DataUnderstanting&DataPreparation.py
@@ -2,163 +2,90 @@ import os
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
+MinDates = []
+MaxDates = []
+FilesLocation = 'Data\\'
 Variables = {
     'Inflaciontotal' : {
-        'FilesName' : 'Inflación total',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
-    },
-    'TasaPoliticaMonetaria' : {
-        'FilesName' : 'Tasa de política monetaria',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
-    },
-    'TasaDesempleo' : {
-        'FilesName' : 'Tasa de desempleo',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
-    },
-    'TasaOcupacion' : {
-        'FilesName' : 'Tasa de ocupación',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
-    },
-    'ConsumoFinaReal' : {
-        'FilesName' : 'Consumo final, real',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
-    },
-    'PIB' : {
-        'FilesName' : 'Producto Interno Bruto (PIB) real, Anual, base: 2015',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
-    },
-    'IPP' : {
-        'FilesName' : 'Índice de Precios del Productor (IPP)',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
-    },
-    'MetaInflacion' : {
-        'FilesName' : 'Meta de inflación',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Inflación total'
     },
     'IPC' : {
-        'FilesName' : 'Índice de Precios al Consumidor',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Índice de Precios al Consumidor'
+    },
+    'TasaPoliticaMonetaria' : {
+        'FilesName' : 'Tasa de política monetaria'
+    },
+    'TasaDesempleo' : {
+        'FilesName' : 'Tasa de desempleo'
+    },
+    'TasaOcupacion' : {
+        'FilesName' : 'Tasa de ocupación'
+    },
+    'ConsumoFinaReal' : {
+        'FilesName' : 'Consumo final, real'
+    },
+    'PIB' : {
+        'FilesName' : 'Producto Interno Bruto (PIB) real, Anual, base: 2015'
+    },
+    'IPP' : {
+        'FilesName' : 'Índice de Precios del Productor (IPP)'
+    },
+    'MetaInflacion' : {
+        'FilesName' : 'Meta de inflación'
     },
     'TRM' : {
-        'FilesName' : 'Tasa Representativa del Mercado (TRM)',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Tasa Representativa del Mercado (TRM)'
     },
     'BaseMonetaria' : {
-        'FilesName' : 'Base monetaria, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Base monetaria, mensual'
     },
     'CuasidinerosTotal' : {
-        'FilesName' : 'Cuasidineros, total, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Cuasidineros, total, mensual'
     },
     'DepositosSistemaFinanciero' : {
-        'FilesName' : 'Depósitos en el sistema financiero, total depósitos, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Depósitos en el sistema financiero, total depósitos, mensual'
     },
     'M1' : {
-        'FilesName' : 'M1, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'M1, mensual'
     },
     'M2' : {
-        'FilesName' : 'M2, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'M2, mensual'
     },
     'M3' : {
-        'FilesName' : 'M3, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'M3, mensual'
     },
     'ReservaBancaria' : {
-        'FilesName' : 'Reserva Bancaria, semanal',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Reserva Bancaria, semanal'
     },
     'TotalCarteraBrutaTitularizacionMonedaExtranjera' : {
-        'FilesName' : 'Total Cartera Bruta sin ajuste por titularización en moneda extranjera , expresada en COP, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Total Cartera Bruta sin ajuste por titularización en moneda extranjera , expresada en COP, mensual'
     },
     'TotalCarteraBrutaTitularizacionMonedaLegal' : {
-        'FilesName' : 'Total Cartera Bruta sin ajuste por titularización en moneda legal, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Total Cartera Bruta sin ajuste por titularización en moneda legal, mensual'
     },
     'CreditoConsumoTasaInteres' : {
-        'FilesName' : 'Crédito de consumo, Tasa de interés',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Crédito de consumo, Tasa de interés'
     },
     'DTF' : {
-        'FilesName' : 'Tasa de Depósitos a Término Fijo (DTF) a 90 días, mensual',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Tasa de Depósitos a Término Fijo (DTF) a 90 días, mensual'
     },
     'TasaInteresCeroCuponTES' : {
-        'FilesName' : 'Tasa de interés Cero Cupón, Títulos de Tesorería (TES), pesos - 1 año',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Tasa de interés Cero Cupón, Títulos de Tesorería (TES), pesos - 1 año'
     },
     'TasaInteresCeroCuponUVR' : {
-        'FilesName' : 'Tasa de interés Cero Cupón, Títulos de Tesorería (TES), UVR - 1 año',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Tasa de interés Cero Cupón, Títulos de Tesorería (TES), UVR - 1 año'
     },
     'TasaInteresColocacionBanRep' : {
-        'FilesName' : 'Tasa de interés de colocación Banco de la República',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Tasa de interés de colocación Banco de la República'
     },
     'TIB' : {
-        'FilesName' : 'Tasa interbancaria (TIB)',
-        'DataFrameOriginal' : None,
-        'Frequency' : None,
-        'DataFrameMonthly' : None
+        'FilesName' : 'Tasa interbancaria (TIB)'
     }
 }
 
-def DataImport():
-    FilesLocation = 'Data\\'
-
+def DataImport(): 
     for NameTimeSerie, DictTimeSerie in Variables.items():
         # Collect initial data
         DictTimeSerie['DataFrameOriginal'] = pd.read_csv(os.path.join(FilesLocation, DictTimeSerie['FilesName'].replace(':','') + '.csv'), 
@@ -182,50 +109,122 @@ def DataImport():
     # print(*Variables, sep='\n')
 
 def DescribeData(BeforeFrequencyModification):
+    fig = plt.figure(figsize=(15, 15))
+    i = 1
 
     for NameTimeSerie, DictTimeSerie in Variables.items():
         if BeforeFrequencyModification:
             print(DictTimeSerie['DataFrameOriginal'].describe())
-            plt.DictTimeSerie['DataFrameOriginal'].hist()
+            ax = plt.subplot(3, 3, i)
+            ax.plot(DictTimeSerie['DataFrameOriginal'])
+            ax.set_title(NameTimeSerie)
+            i += 1
         else:
+
             if DictTimeSerie['Frequency'] != 'M':   #With frequency modification
                 print(DictTimeSerie['DataFrameOriginal'].describe())
-                DictTimeSerie['DataFrameOriginal'].hist()
+                ax = plt.subplot(1, 2, 1)
+                ax.plot(DictTimeSerie['DataFrameOriginal'])
+                ax.set_title(NameTimeSerie)
+
                 print('After frequency modification')
                 print(DictTimeSerie['DataFrameMonthly'].describe())
-                DictTimeSerie['DataFrameMonthly'].hist()
+                ax = plt.subplot(1, 2, 2)
+                ax.plot(DictTimeSerie['DataFrameMonthly'])
+                ax.set_title('After frequency modification')
 
+                plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.4)
+                # plt.show()
+
+        if i == 10 and BeforeFrequencyModification:
+            plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.4)
+            # plt.show()
+            i = 1
+    
+    if BeforeFrequencyModification:
+        plt.subplots_adjust(left=0.1,
+                bottom=0.1, 
+                right=0.9, 
+                top=0.9, 
+                wspace=0.4, 
+                hspace=0.4)
+        # plt.show()
+    
 def VerifyDataQuality():
     for NameTimeSerie, DictTimeSerie in Variables.items():
         print(f"La variable {NameTimeSerie} contiene {DictTimeSerie['DataFrameOriginal'].isnull().sum().sum()} valores faltantes")
 
 def FrequencyModification():
-    MinDate = []
-    MaxDate = []
 
     for NameTimeSerie, DictTimeSerie in Variables.items():
 
         if DictTimeSerie['Frequency'] != 'M':
             if DictTimeSerie['Frequency'] == 'D' or DictTimeSerie['Frequency'] == 'Not regular weekly frequency' or DictTimeSerie['Frequency'] == 'Workdays':
-                ####################################################################### PREGUNTAR
+                #######################################PREGUNTAR
                 DictTimeSerie['DataFrameMonthly'] = DictTimeSerie['DataFrameOriginal'].resample('M').mean()
-                MinDate.append(DictTimeSerie['DataFrameMonthly'].index.min())
-                MaxDate.append(DictTimeSerie['DataFrameMonthly'].index.max())
+                MinDates.append(DictTimeSerie['DataFrameMonthly'].index.min())
+                MaxDates.append(DictTimeSerie['DataFrameMonthly'].index.max())
                 print(f"La variable {NameTimeSerie} posee una frecuencia: {DictTimeSerie['Frequency']}")
 
             elif DictTimeSerie['Frequency'].find('Q') != -1 or DictTimeSerie['Frequency'].find('A') != -1 or DictTimeSerie['Frequency'] == 'Need for filling':
                 DictTimeSerie['DataFrameMonthly'] = DictTimeSerie['DataFrameOriginal'].asfreq('M', method='ffill')
-                MinDate.append(DictTimeSerie['DataFrameMonthly'].index.min())
-                MaxDate.append(DictTimeSerie['DataFrameMonthly'].index.max())
+                MinDates.append(DictTimeSerie['DataFrameMonthly'].index.min())
+                MaxDates.append(DictTimeSerie['DataFrameMonthly'].index.max())
 
             else:
                 print(DictTimeSerie['DataFrameOriginal'].tail(10))
         else:
-            MinDate.append(DictTimeSerie['DataFrameOriginal'].index.min())
-            MaxDate.append(DictTimeSerie['DataFrameOriginal'].index.max())
-    
-    print(f"La fecha más antigua que comparten todas las series de tiempo es {max(MinDate)}, la fecha más reciente que comparten todas las series de tiempo es {min(MaxDate)}")
-    print(f"La fecha más antigua de todas las series de tiempo es {min(MinDate)}, la fecha más reciente de todas las series de tiempo es {max(MaxDate)}")
+            MinDates.append(DictTimeSerie['DataFrameOriginal'].index.min())
+            MaxDates.append(DictTimeSerie['DataFrameOriginal'].index.max())
+
+    globals()['MinDate'] = max(MinDates)
+    globals()['MaxDate'] = min(MaxDates)
+
+    print(f"La fecha más antigua que comparten todas las series de tiempo es {MinDate}, la fecha más reciente que comparten todas las series de tiempo es {MaxDate}")
+    print(f"La fecha más antigua de todas las series de tiempo es {min(MinDates)}, la fecha más reciente de todas las series de tiempo es {max(MaxDates)}")
+
+def DatasetCreation():
+    i = 0
+
+    for NameTimeSerie, DictTimeSerie in Variables.items():
+        if DictTimeSerie['Frequency'] != 'M':   #With frequency modification
+            # Serie between min date and max date
+            SubSet = DictTimeSerie['DataFrameMonthly'][MinDate:MaxDate]
+        else:
+            # Serie between min date and max date
+            SubSet = DictTimeSerie['DataFrameOriginal'][MinDate:MaxDate]
+
+        # Normalize
+        ######################## PREGUNTAR normalización de variables TasaPolíticaMonetaria, PIB, MetaInflacion
+        Scaler = MinMaxScaler(feature_range=(0, 1))
+        DictTimeSerie['DataFrameFinal'] = pd.DataFrame(Scaler.fit_transform(SubSet),
+                                                index=SubSet.index,
+                                                columns=SubSet.columns)
+        i += 1
+
+        # Final Dataset
+        if i == 1:
+            globals()['Dataset'] = SubSet
+            globals()['NormalizedDataset'] = DictTimeSerie['DataFrameFinal']
+        else:
+            globals()['Dataset'] = pd.merge_ordered(Dataset, SubSet, on='Fecha')
+            globals()['NormalizedDataset'] = pd.merge_ordered(NormalizedDataset, DictTimeSerie['DataFrameFinal'], on='Fecha')
+
+    print(Dataset)  
+    print(NormalizedDataset)
+    # Save Dataset
+    Dataset.to_csv(os.path.join(FilesLocation, 'Dataset.csv'))
+    NormalizedDataset.to_csv(os.path.join(FilesLocation, 'NormalizedDataset.csv'))
 
 def run():
     DataImport()
@@ -233,57 +232,7 @@ def run():
     VerifyDataQuality()
     FrequencyModification()
     DescribeData(BeforeFrequencyModification=False)
+    DatasetCreation()
 
 if __name__ == '__main__':
-    run()   
-
-
-# # List of [FilesNames, DataFrameNames, DataFrameOriginal, Frequency, DataFrameMonthly]
-# Variables = [['Inflación total', 'Inflaciontotal', ], 
-#     ['Tasa de política monetaria', 'TasaPoliticaMonetaria', ], 
-#     ['Tasa de desempleo', 'TasaDesempleo', ], 
-#     ['Tasa de ocupación', 'TasaOcupacion', ], 
-#     ['Consumo final, real', 'ConsumoFinaReal', ], 
-#     ['Producto Interno Bruto (PIB) real, Anual, base: 2015', 'PIB', ], 
-#     ['Índice de Precios del Productor (IPP)', 'IPP', ], 
-#     ['Meta de inflación', 'MetaInflacion', ], 
-#     ['Índice de Precios al Consumidor', 'IPC', ], 
-#     ['Tasa Representativa del Mercado (TRM)', 'TRM', ], 
-#     ['Base monetaria, mensual', 'BaseMonetaria', ], 
-#     ['Cuasidineros, total, mensual', 'CuasidinerosTotal', ], 
-#     ['Depósitos en el sistema financiero, total depósitos, mensual', 'DepositosSistemaFinanciero', ],
-#     ['M1, mensual', 'M1', ], 
-#     ['M2, mensual', 'M2', ], 
-#     ['M3, mensual', 'M3', ], 
-#     ['Reserva Bancaria, semanal', 'ReservaBancaria', ], 
-#     ['Total Cartera Bruta sin ajuste por titularización en moneda extranjera , expresada en COP, mensual', 'TotalCarteraBrutaTtitularizacionMonedaExtranjera', ], 
-#     ['Total Cartera Bruta sin ajuste por titularización en moneda legal, mensual',  'TotalCarteraBrutaTtitularizacionMonedaLegal', ], 
-#     ['Crédito de consumo, Tasa de interés', 'CreditoConsumoTasaInteres', ], 
-#     ['Tasa de Depósitos a Término Fijo (DTF) a 90 días, mensual', 'DTF', ], 
-#     ['Tasa de interés Cero Cupón, Títulos de Tesorería (TES), pesos - 1 año', 'TasaInteresCeroCuponTES', ], 
-#     ['Tasa de interés Cero Cupón, Títulos de Tesorería (TES), UVR - 1 año', 'TasaInteresCeroCuponUVR', ], 
-#     ['Tasa de interés de colocación Banco de la República', 'TasaInteresColocacionBanRep', ], 
-#     ['Tasa interbancaria (TIB)', 'TIB', ], 
-#     ]
-
-# exec(f"{Variable[1]}= pd.read_csv(os.path.join(FilesLocation, NameTimeSerie.replace(':','') + '.csv'), sep=';')')
-#  [['Inflación total', 'Inflaciontotal', 'M'], ['Tasa de política monetaria', 'TasaPoliticaMonetaria', 'D'], ['Tasa de desempleo', 'TasaDesempleo', 'M'], ['Tasa de ocupación', 'TasaOcupacion', 'M'], ['Consumo final, real', 'ConsumoFinaReal', 'Q-DEC'], ['Producto Interno Bruto (PIB) real, Anual, base: 
-# 2015', 'PIB', 'A-DEC'], ['Índice de Precios del Productor (IPP)', 'IPP', 'M'], ['Meta de inflación', 'MetaInflacion', 'Mensual, Completar últimos meses'], ['Índice de Precios al Consumidor', 'IPC', 'M'], ['Tasa Representativa del Mercado (TRM)', 'TRM', 'D'], ['Base monetaria, mensual', 'BaseMonetaria', 'M'], ['Cuasidineros, total, mensual', 'CuasidinerosTotal', 'M'], ['Depósitos en el sistema financiero, total depósitos, mensual', 'DepositosSistemaFinanciero', 'M'], ['M1, mensual', 'M1', 'M'], ['M2, mensual', 'M2', 'M'], ['M3, mensual', 'M3', 'M'], ['Reserva Bancaria, semanal', 'ReservaBancaria', 'Semanal pero no pareja'], ['Total Cartera Bruta sin ajuste por titularización en moneda extranjera , expresada en COP, mensual', 'TotalCarteraBrutaTtitularizacionMonedaExtranjera', 'M'], ['Total Cartera Bruta sin ajuste por titularización en moneda legal, mensual', 'TotalCarteraBrutaTtitularizacionMonedaLegal', 'M'], ['Crédito de consumo, Tasa de interés', 'CréditoConsumoTasaInteres', 'M'], ['Tasa de Depósitos a Término Fijo (DTF) a 90 días, mensual', 'DTF', 'M'], ['Tasa de interés Cero Cupón, Títulos de Tesorería (TES), pesos - 1 año', 'TasaInteresCeroCuponTES', 'Diario, hábiles'], ['Tasa de interés Cero Cupón, Títulos de Tesorería (TES), UVR - 1 año', 'TasaInteresCeroCuponUVR', 'Diario, hábiles'], ['Tasa de interés de colocación Banco de la República', 'TasaInteresColocacionBanRep', 'M'], ['Tasa interbancaria (TIB)', 'TIB', 'Diario, hábiles']]                                                    
-
-
-
-        # Read the csv files
-        # The headers are settled like 'Fecha', Name of the dataframe
-        # globals()[Variable[1]] = pd.read_csv(os.path.join(FilesLocation, NameTimeSerie.replace(':','') + '.csv'), 
-        #                                 sep=';', 
-        #                                 names=['Fecha', Variable[1]], 
-        #                                 skiprows=1, 
-        #                                 dtype={'Fecha': 'str', Variable[1]: 'float'}, 
-        #                                 parse_dates=['Fecha'], 
-        #                                 index_col=0, 
-        #                                 decimal=',')  
-
-
-        # print(globals()[Variable[1]].tail())
-        # print(pd.infer_freq(globals()[Variable[1]]['Fecha']))
-        
+    run()
